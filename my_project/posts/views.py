59adcd0e7e1579085cec8cffc0f8c00abedbb4bd
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-16 09:40:27 +0000

    add views for serializers

--- my_project/posts/views.py
+++ my_project/posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import posts, WorkoutPlan
+from .serializers import PostSerializer, WorkoutPlanSerializer
 
-# Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class WorkoutPlanViewSet(viewsets.ModelViewSet):
+    queryset = WorkoutPlan.objects.all()
+    serializer_class = WorkoutPlanSerializer
