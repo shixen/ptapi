9ecd8eff49324212f0bf3c9d2d062a806312e0ee
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-16 13:35:17 +0000

    change serializer to new app

--- my_project/posts/serializers.py
+++ my_project/posts/serializers.py
@@ -1,12 +1,7 @@
 from rest_framework import serializers
-from .models import Post, WorkoutPlan
+from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = '__all__'
-
-class WorkoutPlanSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = WorkoutPlan
         fields = '__all__'
