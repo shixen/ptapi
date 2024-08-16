231bb6a58dda260b225169f49b1c0d03226af0a2
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-16 13:35:31 +0000

    change view to new app

--- my_project/posts/views.py
+++ my_project/posts/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Post, WorkoutPlan
-from .serializers import PostSerializer, WorkoutPlanSerializer
+from .models import Post
+from .serializers import PostSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-
-class WorkoutPlanViewSet(viewsets.ModelViewSet):
-    queryset = WorkoutPlan.objects.all()
-    serializer_class = WorkoutPlanSerializer
