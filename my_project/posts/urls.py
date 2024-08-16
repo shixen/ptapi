05ace36c7620687fa3ac2f8c66f3b94f7c5345c6
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-16 12:33:59 +0000

    change url route

--- my_project/posts/urls.py
+++ my_project/posts/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, WorkoutPlanViewSet
+from .views import PostViewSet
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
-router.register(r'workoutplans', WorkoutPlanViewSet)
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('', include(router.urls)), 
 ]
