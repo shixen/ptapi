a5e45d94f306d0e71a6d92d328a71da8b8928370
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-16 13:35:03 +0000

    change model to new app

--- my_project/posts/models.py
+++ my_project/posts/models.py
@@ -1,21 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
-
-
-class WorkoutPlan(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    exercises = models.TextField()
-    created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.title
