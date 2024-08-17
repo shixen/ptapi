498c2f120254e7d53b6827e0b10945a838c03e20
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-17 08:51:46 +0000

    add permissions

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from rest_framework import status
 from .serializers import UserSerializer
 
 
 class SignupView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+    
 
     def postsign(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
