b4c3afa06bb38889bed6fa79cf60482858c10545
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-17 11:52:06 +0000

    add login/logout to views

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,37 @@
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
-from .serializers import UserSerializer
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from django.contrib.auth import authenticate
+from .serializers import UserSerializer, LoginSerializer
 
 class SignupViewSet(viewsets.GenericViewSet, viewsets.mixins.CreateModelMixin):
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+#using REST token to handle login 
+class CustomLoginView(ObtainAuthToken):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = authenticate(username=serializer.validated_data['username'], password=serializer.validated_data['password'])
+        if user is None:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key}, status=status.HTTP_200_OK)
+
+#REST token to handle logout
+class LogoutView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response({'message': 'Successfully logged out'}, status=status.HTTP_200_OK)
