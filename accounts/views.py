6a4e8487e1998bb7fa6b147b517647d21cb1f152
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-17 10:04:14 +0000

    remove unneccesary kode

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,7 @@
-from django.shortcuts import render
-from rest_framework import generics
-from rest_framework.response import Response
+from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
-from rest_framework import status
 from .serializers import UserSerializer
 
-
-class SignupView(generics.CreateAPIView):
+class SignupViewSet(viewsets.GenericViewSet, viewsets.mixins.CreateModelMixin):
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
-    
-
-    def postsign(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            return Response({'message': 'User created'}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
