c40b0d2d680f9d579da11e96e42a48e982dcddf8
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-17 10:04:23 +0000

    move url path

--- my_project/urls.py
+++ my_project/urls.py
@@ -19,7 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('', include('posts.urls')),
     path('workoutplan/', include('workoutplan.urls')),
-    path('accounts/', include('accounts.urls')),
+    
 ]
