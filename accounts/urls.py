09b12ff71f77499f7fc13c39e72e158ee06c9e94
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-17 12:06:05 +0000

    add urls to login/logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import SignupViewSet
+from .views import SignupViewSet, CustomLoginView, LogoutView
 
 router = DefaultRouter()
 router.register(r'signup', SignupViewSet, basename='signup')
 
+#urls to accounts management
 urlpatterns = [
     path('', include(router.urls)),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
