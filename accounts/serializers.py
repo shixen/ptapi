721d423e78b3c44bda5e8872ca0914d3a211ce63
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-17 11:51:54 +0000

    add serializers to handle login

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -14,3 +15,20 @@ class UserSerializer(serializers.ModelSerializer):
             password=validated_data['password']
         )
         return user
+
+# token serializer to handle login information
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+
+    def validate(self, data):
+        user = authenticate(username=data['username'], password=data['password'])
+        if user is None:
+            raise serializers.ValidationError("Invalid credentials")
+        return data
+
+    def create(self, validated_data):
+        user = User.objects.get(username=validated_data['username'])
+        token, created = Token.objects.get_or_create(user=user)
+        return token.key
