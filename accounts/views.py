9d03bd3bf5a9d0b9ea26bdd851edc9589d303e3d
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-17 13:55:02 +0000

    change structure of class and add error handling

--- accounts/views.py
+++ accounts/views.py
@@ -7,7 +7,7 @@ from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 from .serializers import UserSerializer, LoginSerializer
 
 class SignupViewSet(viewsets.GenericViewSet, viewsets.mixins.CreateModelMixin):
@@ -17,17 +17,18 @@ class SignupViewSet(viewsets.GenericViewSet, viewsets.mixins.CreateModelMixin):
 #using REST token to handle login 
 class CustomLoginView(APIView):
     def post(self, request):
-        serializer = LoginSerializer(data=request.data)
-        if serializer.is_valid():
-            username = serializer.validated_data['username']
-            password = serializer.validated_data['password']
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return Response({'detail': 'Login successful'}, status=status.HTTP_200_OK)
-            else:
-                return Response({'detail': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        username = request.data.get('username')
+        password = request.data.get('password')
+        
+        if not username or not password:
+            return Response({'error': 'Username and password are required.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return Response({'status': 'User logged in'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
 #REST token to handle logout
 class LogoutView(APIView):
