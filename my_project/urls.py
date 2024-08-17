84174f29f2939b1ee521338a106adc5e16590417
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-17 08:51:52 +0000

    change url path

--- my_project/urls.py
+++ my_project/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls')),
     path('workoutplan/', include('workoutplan.urls')),
-    path('api/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
