d64db0c35205be91f329333a473533bea5dcbe0c
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-26 18:44:47 +0000

    add error handeling

--- accounts/views.py
+++ accounts/views.py
@@ -7,7 +7,7 @@ from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from .serializers import UserSerializer, LoginSerializer
 
 class SignupViewSet(viewsets.GenericViewSet, viewsets.mixins.CreateModelMixin):
@@ -22,23 +22,26 @@ class CustomLoginView(APIView):
             username = serializer.validated_data['username']
             password = serializer.validated_data['password']
             
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
+            try:
+                user = authenticate(username=username, password=password)
+                if user is not None:
+                    login(request, user)
 
-                token, created = Token.objects.get_or_create(user=user)
-                
-                return Response({'token': token.key}, status=status.HTTP_200_OK)
-            else:
-                return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+                    token, created = Token.objects.get_or_create(user=user)
+                    
+                    return Response({'token': token.key}, status=status.HTTP_200_OK)
+                else:
+                    return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+            except Exception as e:
+                print(f"Login error: {e}")
+                return Response({'error': 'An unexpected error occurred'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#REST token to handle logout
+
 class LogoutView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
-
         try:
             token = Token.objects.get(user=request.user)
             token.delete()
