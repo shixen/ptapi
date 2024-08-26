5869b7f20f1ad054c1ecfe216454c4ee5aafa8d7
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-26 18:44:28 +0000

    add authtoken to installed apps

--- my_project/settings.py
+++ my_project/settings.py
@@ -72,6 +72,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
     'posts',
     'workoutplan',
