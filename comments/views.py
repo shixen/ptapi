3909d537591095c56999c98cdc5f0a50dd2f1afb
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-27 15:41:04 +0000

    add filter to comment posts

--- comments/views.py
+++ comments/views.py
@@ -4,9 +4,14 @@ from .models import Comment
 from .serializers import CommentSerializer
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        post_id = self.request.query_params.get('post')
+        if post_id:
+            return Comment.objects.filter(post_id=post_id)
+        return Comment.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
