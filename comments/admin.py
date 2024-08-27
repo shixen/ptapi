70b57af6daed9d35e0cf80b59c38373f17c33b36
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-27 16:07:22 +0000

    change order of list fields

--- comments/admin.py
+++ comments/admin.py
@@ -3,6 +3,7 @@ from .models import Comment
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'author', 'text', 'created_at', 'workout')
-    search_fields = ('author', 'text')
-    list_filter = ('created_at', 'workout')
+    list_display = ('id', 'owner', 'post', 'content', 'created_at')
+    search_fields = ('content', 'owner__username', 'post__title')
+    list_filter = ('created_at', 'post')
+    ordering = ('-created_at',)
