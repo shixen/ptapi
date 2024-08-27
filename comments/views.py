aaacd2a68cd2cbd4d78ae2a9c44d2e16edbd6a43
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-27 15:46:15 +0000

    add queryset to commentview

--- comments/views.py
+++ comments/views.py
@@ -4,6 +4,7 @@ from .models import Comment
 from .serializers import CommentSerializer
 
 class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
