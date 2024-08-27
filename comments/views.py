f3404697b282243768614da4c0d6b4b6d7970b51
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-27 15:32:47 +0000

    add comment viewset

---
+++ comments/views.py
@@ -0,0 +1,12 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Comment
+from .serializers import CommentSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
