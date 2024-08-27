ec6cdd50bd7356bdda1cf19fc6f77e80ad56b177
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-27 15:33:42 +0000

    comments url to main url

--- my_project/urls.py
+++ my_project/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('', include('posts.urls')),
+    path('', include('comments.urls')), 
     path('workoutplan/', include('workoutplan.urls')),
     
 ]
